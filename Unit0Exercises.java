1f053d77e766f0b5230857b79554e967c28fbed8
csuh3511 <csuh1@hwemail.com>
2026-09-02 13:46:39 -0700

    3

--- Unit0Exercises.java
+++ Unit0Exercises.java
@@ -1,22 +1,20 @@
 public class Unit0Exercises {
     public static void main(String[] args) {
         int score = 85;
-        score += 5;
         double price = 19.99;
-                price += 2.50;
 
-        boolean isPassing = true;
-        final double TAX_RATE = 0.0925;
-        char grade = 'A';
-        System.out.println(grade);
+        // boolean isPassing = true;
+        // final double TAX_RATE = 0.0925;
+        // char grade = 'A';
+        // System.out.println(grade);
 
         System.out.println(" --- Variable Snapshot --- ");
         System.out.println("Score: " + score);
         System.out.println("Price: " + price);
         System.out.println("Passing: " + isPassing);
 
-        score = 90;
-        price = 22.49;
+        score += 5;
+        price += 2.50;
         isPassing = false;
 
         System.out.println(" --- After Updates --- ");
